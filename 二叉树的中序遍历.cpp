c13c51f379016a95260fb7e2c592ed066c2d8d19
Jianyu_Heng <w547170036@163.com>
2016-10-31 23:15:53 +0800

    Update 二叉树的中序遍历.cpp

--- 二叉树的中序遍历.cpp
+++ 二叉树的中序遍历.cpp
@@ -10,6 +10,7 @@
  *     }
  * }
  */
+//非递归
 class Solution {
     /**
      * @param root: The root of binary tree.
@@ -39,4 +40,49 @@ public:
       }
       return result;
       }
+};
+//递归
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in vector which contains node values.
+     */
+public:
+    vector<int> inorderTraversal(TreeNode *root) { 
+        // write your code here 
+        vector<int> result; 
+        if (root == NULL) 
+        { 
+            return result; 
+        }else 
+        { 
+            inorderCore(root, result); 
+        } 
+        return result; 
+    } 
+    void inorderCore(TreeNode *root, vector<int> &result) 
+    { 
+        if (root->left != NULL) 
+        { 
+            inorderCore(root->left, result); 
+        } 
+        result.push_back(root->val); 
+        if (root->right != NULL) 
+        { 
+            inorderCore(root->right, result); 
+        } 
+        return; 
+    } 
 };
