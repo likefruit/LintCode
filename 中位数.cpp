17ede05d2a5e3a0378193736243d781ed2f6884e
Jianyu_Heng <w547170036@163.com>
2016-11-23 20:28:23 +0800

    Create 中位数.cpp

---
+++ 中位数.cpp
@@ -0,0 +1,36 @@
+//我的答案
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers.
+     * @return: An integer denotes the middle number of the array.
+     */
+    int median(vector<int> &nums) {
+        // write your code here
+        sort(nums.begin(),nums.end());
+        
+        return (nums.size()%2)?nums[nums.size()/2]:nums[nums.size()/2-1];
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers.
+     * @return: An integer denotes the middle number of the array.
+     */
+    int median(vector<int> &nums) {
+        auto target = nums.begin() + nums.size() / 2;
+        nth_element(nums.begin(), target, nums.end());
+        if (nums.size() % 2 == 1) {  // nums has odd number of elements.
+            return *target;
+        } else {  // nums has even number of elements.
+            int x = *target;
+            nth_element(nums.begin(), target - 1, nums.end());
+            return *(target - 1);
+        }
+    }
+};
