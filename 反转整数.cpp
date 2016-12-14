19ae3008f6646e8732776a950147c5e68d8df698
Jianyu_Heng <w547170036@163.com>
2016-12-14 16:54:37 +0800

    Create 反转整数.cpp

---
+++ 反转整数.cpp
@@ -0,0 +1,112 @@
+**题目**
+
+将一个整数中的数字进行颠倒，当颠倒后的整数溢出时，返回 0 (标记为 32 位整数)。
+
+**样例**
+
+给定 x = 123，返回 321
+
+给定 x = -123，返回 -321
+
+```
+class Solution {
+public:
+    /**
+     * @param n the integer to be reversed
+     * @return the reversed integer
+     */
+    int reverseInteger(int n) {
+        // Write your code here
+        
+        //如何判断整数的溢出？
+        //如何判断整数反转后的溢出？是不是应该换成long
+        
+        bool flag=true;
+        long res=0;
+        
+        //输入非法
+        if(n>=2147483648||n<=-2147483648)
+            return 0;
+        
+        //反转操作 
+        else
+        {
+            vector<int> vec;
+            if(n<0)
+            {
+                flag=false;
+                n=-n;
+            }
+            while(n!=0)
+            {
+                //得到每一位的数字加入容器尾端
+                vec.push_back(n%10);
+                n/=10;
+            }
+            //求反转后的数字
+            for(auto c:vec)
+                res=res*10+c;
+        }
+    
+        //反转之后的判断
+        if(res>=2147483648)
+        {
+            return 0;
+        }
+        else if(flag==false)
+            return -res;
+        else
+            return res;
+    }
+};
+```
+
+
+----------
+
+
+**优化**
+
+我们可以一边计算res的值一边判断res是否越界
+
+```
+class Solution {
+public:
+    /**
+     * @param n the integer to be reversed
+     * @return the reversed integer
+     */
+    int reverseInteger(int n) {
+        // Write your code here
+        
+        //如何判断整数的溢出？
+        //如何判断整数反转后的溢出？是不是应该换成long
+ 
+        long res=0;
+        
+        //输入非法
+        if(n>=2147483648||n<=-2147483648)
+            return 0;
+        
+        //反转操作 
+        else
+        {
+            vector<int> vec;
+            while(n!=0)
+            {
+                //得到每一位的数字加入容器尾端
+                vec.push_back(n%10);
+                n/=10;
+            }
+            //求反转后的数字，并判断数字是否越界
+            for(auto c:vec)
+            {
+                res=res*10+c;
+                if(res>=2147483648)
+                    return 0;
+            }
+        }
+        return res;
+    }
+};
+```
